b3050779059c66c9541fb2a93a5ed99743b6e4dd
hari <hariprasadm@auruminfosol.com>
2018-03-29 21:01:36 +0530

    Added test cases

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name="kiteutils",
+	version="0.1",
+	description="",
+	author="Hari",
+	author_email="hari.kongu@gmail.com",
+	url="",
+	packages=["kiteutils"],
+	download_url="https://github.com/harikongu/kite_utils",
+	license="MIT",
+	classifiers=[
+
+	],
+	install_requires=[]
+)
--- tests.py
+++ tests.py
@@ -7,6 +7,14 @@ class KiteUtilsTest(unittest.TestCase):
     def test_profit(self):
         self.assertEqual(KiteUtil.get_profit(100, 110, 1, KiteUtil.EXCHANGE_NSE, KiteUtil.TRADE_TYPE_INTRADAY), (9.944675, 0.055325))
 
+    def test_market_hour(self):
+        # Before 9:00 AM or After 3:30 PM
+        self.assertEqual(KiteUtil.is_market_hour(), False)
+
+    def test_timezone(self):
+        # Confirm system timezone is IST
+        self.assertEqual(KiteUtil.is_indian_timezone(), True)
+
 if __name__ == '__main__':
     unittest.main()
 

7a75d93b04e7b9232de41220c74a8f9b00288538
hari <hariprasadm@auruminfosol.com>
2018-03-29 20:00:38 +0530

    Changed methods to staticmethod

--- kite_utils.py
+++ kite_utils.py
@@ -51,76 +51,81 @@ class KiteUtil(object):
                     '23-11-2018',  # 15	Gurunanak Jayanti
                     '25-12-2018']  # 16	Christmas
 
-    def get_profit(self, buy_price, sell_price, qty, exchange, trade_type):
+    @staticmethod
+    def get_profit(buy_price, sell_price, qty, exchange, trade_type):
         # price_diff = (sell_price - buy_price) * QTY
         total_buy_price = buy_price * qty
         total_sell_price = sell_price * qty
 
-        if exchange == self.EXCHANGE_BSE:
+        if exchange == KiteUtil.EXCHANGE_BSE:
             transaction_charges = 3
-        elif exchange == self.EXCHANGE_NSE:
-            buy_transaction = total_buy_price * self.TRANSACTION_CHARGES_NSE
-            sell_transaction = total_sell_price * self.TRANSACTION_CHARGES_NSE
+        elif exchange == KiteUtil.EXCHANGE_NSE:
+            buy_transaction = total_buy_price * KiteUtil.TRANSACTION_CHARGES_NSE
+            sell_transaction = total_sell_price * KiteUtil.TRANSACTION_CHARGES_NSE
             transaction_charges = buy_transaction + sell_transaction
 
-        if trade_type == self.TRADE_TYPE_INTRADAY:
-            buy_brokerage = total_buy_price * self.BROKERAGE_INTRADAY
-            sell_brokerage = total_sell_price * self.BROKERAGE_INTRADAY
+        if trade_type == KiteUtil.TRADE_TYPE_INTRADAY:
+            buy_brokerage = total_buy_price * KiteUtil.BROKERAGE_INTRADAY
+            sell_brokerage = total_sell_price * KiteUtil.BROKERAGE_INTRADAY
 
-            buy_brokerage = buy_brokerage if buy_brokerage < self.BROKERAGE_INTRADAY_FLAT else self.BROKERAGE_INTRADAY_FLAT
-            sell_brokerage = sell_brokerage if sell_brokerage < self.BROKERAGE_INTRADAY_FLAT else self.BROKERAGE_INTRADAY_FLAT
+            buy_brokerage = buy_brokerage if buy_brokerage < KiteUtil.BROKERAGE_INTRADAY_FLAT else KiteUtil.BROKERAGE_INTRADAY_FLAT
+            sell_brokerage = sell_brokerage if sell_brokerage < KiteUtil.BROKERAGE_INTRADAY_FLAT else KiteUtil.BROKERAGE_INTRADAY_FLAT
 
             brokerage = buy_brokerage + sell_brokerage
-            stt_ctt = sell_price * self.STT_CTT_INTRADAY
-        elif trade_type == self.TRADE_TYPE_DELIVERY:
-            stt_ctt = (sell_price * self.STT_CTT_DELIVERY) + (buy_price * self.STT_CTT_DELIVERY)
+            stt_ctt = sell_price * KiteUtil.STT_CTT_INTRADAY
+        elif trade_type == KiteUtil.TRADE_TYPE_DELIVERY:
+            stt_ctt = (sell_price * KiteUtil.STT_CTT_DELIVERY) + (buy_price * KiteUtil.STT_CTT_DELIVERY)
             brokerage = 0
 
-        gst = (brokerage + transaction_charges) * self.GST
-        sebi_buy_charges = buy_price * qty * self.SEBI_CHARGES
-        sebi_sell_charges = sell_price * qty * self.SEBI_CHARGES
+        gst = (brokerage + transaction_charges) * KiteUtil.GST
+        sebi_buy_charges = buy_price * qty * KiteUtil.SEBI_CHARGES
+        sebi_sell_charges = sell_price * qty * KiteUtil.SEBI_CHARGES
         sebi_charges = sebi_buy_charges + sebi_sell_charges
 
         total_charges = brokerage + transaction_charges + stt_ctt + gst + sebi_charges
-        profit_loss = total_buy_price - total_sell_price
+        profit_loss = total_sell_price - total_buy_price
         final_profit_loss = profit_loss - total_charges
 
         return (final_profit_loss, total_charges)
 
-    def is_tradingday(self):
+    @staticmethod
+    def is_tradingday():
         today = date.today()
         weekday_index = today.weekday()
         # Saturday and Sunday
         if weekday_index == 5 or weekday_index == 6:
             return False
         else:
-            if today in [datetime.strptime(d, "%d/%m/%Y").date() for d in self.holiday_list]:
+            if today in [datetime.strptime(d, "%d/%m/%Y").date() for d in KiteUtil.holiday_list]:
                 return False
             else:
                 return True
 
-    def is_indian_timezone(self):
+    @staticmethod
+    def is_indian_timezone():
         if tzname[1] == 'IST':
             return True
         else:
             return False
 
-    def is_pre_market_hour(self):
+    @staticmethod
+    def is_pre_market_hour():
         now = datetime.now()
 
-        pre_market_open_time = time(*map(int, self.PRE_MARKET_OPEN_TIME.split(':')))
-        pre_market_close_time = time(*map(int, self.PHYSICAL_MARKET_OPEN_TIME.split(':')))
+        pre_market_open_time = time(*map(int, KiteUtil.PRE_MARKET_OPEN_TIME.split(':')))
+        pre_market_close_time = time(*map(int, KiteUtil.PHYSICAL_MARKET_OPEN_TIME.split(':')))
 
         if pre_market_open_time <= now.time() <= pre_market_close_time:
             return True
         else:
             return False
 
-    def is_market_hour(self):
+    @staticmethod
+    def is_market_hour():
         now = datetime.now()
 
-        physical_market_open_time = time(*map(int, self.PHYSICAL_MARKET_OPEN_TIME.split(':')))
-        physical_market_close_time = time(*map(int, self.PHYSICAL_MARKET_CLOSE_TIME.split(':')))
+        physical_market_open_time = time(*map(int, KiteUtil.PHYSICAL_MARKET_OPEN_TIME.split(':')))
+        physical_market_close_time = time(*map(int, KiteUtil.PHYSICAL_MARKET_CLOSE_TIME.split(':')))
 
         if physical_market_open_time <= now.time() <= physical_market_close_time:
             return True
